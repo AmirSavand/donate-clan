c3f046c10dd3d6fa740793729699e0f85d3f1b27
Amir Savand <amir@savandbros.com>
2018-03-01 02:40:29 +0300

    Update code format and check for null data

--- app/components/user/user_controller.js
+++ app/components/user/user_controller.js
@@ -3,15 +3,14 @@
 app.controller("UserController", function (Member, API, $scope, $stateParams) {
 
   function constructor() {
+
+    // Get user from param
     $scope.user = $stateParams.user;
 
+    // Get user from API
     if (!$scope.user) {
       API.Users.get({ username: $stateParams.username }, function (data) {
         $scope.user = data;
-
-        // if ($scope.user.member) {
-        //   $scope.user.member = new Member($scope.user.member);
-        // }
       });
     }
   }
