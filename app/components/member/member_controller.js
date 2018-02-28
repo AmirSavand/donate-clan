1f078fb2eb6940a47f3aa484fac2f11a079343f1
Amir Savand <amir@savandbros.com>
2018-03-01 01:07:18 +0300

    Delete comment

--- app/components/member/member_controller.js
+++ app/components/member/member_controller.js
@@ -13,31 +13,16 @@ app.controller("MemberController", function (API, Member, toaster, $scope, $stat
       color: "success",
       title: "Arena",
       value: member.arena.name
-        // }, {
-        //   icon: "star-half-o",
-        //   color: "warning",
-        //   title: "Score",
-        //   value: member.get.score
     }, {
       icon: "trophy",
       color: "warning",
       title: "Max Trophies",
       value: member.stats.maxTrophies
-        // }, {
-        //   icon: "plus-circle",
-        //   color: "success",
-        //   title: "Most Donation",
-        //   value: "+" + member.get.most_donated
-        // }, {
-        //   icon: "trophy",
-        //   color: "warning",
-        //   title: "1st Clan Chest",
-        //   value: member.get.clan_chest_champion
     }];
   }
 
   function constructor() {
-    
+
     $scope.member = $stateParams.member;
 
     API.Member.get({ tag: $stateParams.tag, keys: "name,tag,clan,arena,stats" }, function (data) {
