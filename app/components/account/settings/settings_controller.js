3b6b8b28cf81ae36094a97463e15d0499278da94
Amir Savand <amir@savandbros.com>
2018-03-01 03:22:13 +0300

    Settings update

--- app/components/account/settings/settings_controller.js
+++ app/components/account/settings/settings_controller.js
@@ -2,11 +2,37 @@
 
 app.controller("SettingsController", function (Auth, API, $scope, $state) {
 
-  function constructor() {
+  var user = Auth.getAuth();
 
-    // Get auth
-    $scope.user = Auth.getAuth();
+  function constructor() {
+    API.Users.get({ username: user.username }, function (data) {
+      $scope.form.data = {
+        email: data.email,
+        about: data.about,
+        member: data.member,
+        link: data.link,
+        nationality: data.nationality,
+        picture: data.picture,
+        username: data.username
+      };
+    });
   }
+
+  $scope.update = function (form) {
+    form.loading = true;
+
+    API.Users.put({ username: user.username }, form.data,
+      function (data) {
+        form.loading = false;
+        form.error = null;
+        form.data = data;
+      },
+      function (data) {
+        form.loading = false;
+        form.error = data.data;
+      }
+    );
+  };
 
   constructor();
 });
