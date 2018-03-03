b3d17e10987e255d6f560d8bd08a9f1853825196
Amir Savand <amir@savandbros.com>
2018-03-03 21:28:42 +0300

    Update member code format

--- app/components/member/member_controller.js
+++ app/components/member/member_controller.js
@@ -2,32 +2,45 @@
 
 app.controller("MemberController", function (API, Member, toaster, $scope, $state, $stateParams) {
 
-  function generateDetailCards(member) {
+  function constructor() {
+
+    /**
+     * Get basic data from params
+     *
+     * @type {Member}
+     */
+    $scope.member = $stateParams.member;
+
+    /**
+     * Information of a member
+     *
+     * @type {object}
+     */
     $scope.detailCards = [{
       icon: "chess",
       color: "primary",
       title: "Role",
-      value: member.role
+      value: "..."
     }, {
       icon: "flag-checkered",
       color: "success",
       title: "Arena",
-      value: member.arena.name
+      value: "..."
     }, {
       icon: "trophy",
       color: "warning",
       title: "Max Trophies",
-      value: member.stats.maxTrophies
+      value: "..."
     }];
-  }
 
-  function constructor() {
-
-    $scope.member = $stateParams.member;
-
+    /**
+     * Get full member detail via API
+     */
     API.Member.get({ tag: $stateParams.tag, keys: "name,tag,clan,arena,stats" }, function (data) {
       $scope.member = new Member(data);
-      generateDetailCards($scope.member);
+      $scope.detailCards[0].value = $scope.member.role;
+      $scope.detailCards[1].value = $scope.member.arena.name;
+      $scope.detailCards[2].value = $scope.member.stats.maxTrophies;
     });
   }
 
